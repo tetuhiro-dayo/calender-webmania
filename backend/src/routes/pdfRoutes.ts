147345f4b0ed109ad242b940fcc3818b2ea4b054
tetsuhiro-dayo <119085878+tetuhiro-dayo@users.noreply.github.com>
2025-04-14 17:57:56 +0900

    色々

--- backend/src/routes/pdfRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import pdfController from '../controllers/pdfController';
-
-const router = Router();
-
-router.post('/generate', pdfController.generatePDF);
-
-export default router;
