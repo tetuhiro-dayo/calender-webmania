250688f7df8bff595c2ce7a5207329785be7e14c
tetsuhiro-dayo <119085878+tetuhiro-dayo@users.noreply.github.com>
2025-04-20 18:49:00 +0900

    レダイレクトをいい感じに

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { redirect } from "next/navigation";
 
 export default function HomePage() {
     const now = new Date();
-    redirect(`/month/${now.getFullYear()}-${now.getMonth() + 1}`);
+    const year = String(now.getFullYear()).padStart(4, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    redirect(`/month/${year}-${month}`);
 
     return null;
 }
